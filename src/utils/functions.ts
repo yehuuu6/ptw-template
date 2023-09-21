9bf0406c84740c956791186e11d41be46113485f
sscorpi <therenaydin@gmail.com>
2023-09-22 01:23:19 +0300

    Added loader + changed urls

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,39 +1,49 @@
 import axios from "axios";
-import { url } from "../index";
+import { todoURL, customURL } from "@/core/index";
+
+const apiResponseDiv = document.getElementById(
+  "api-response"
+) as HTMLDivElement;
 
 async function sendApiRequest(url: string, formData: FormData) {
-    try {
-      const response = await axios({
-        url: url,
-        method: "post",
-        data: formData,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          "Content-Type": "multipart/form-data",
-        },
-      });
+  try {
+    const response = await axios({
+      url: url,
+      method: "post",
+      data: formData,
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "multipart/form-data",
+      },
+    });
 
-      return response.data;
-    } catch (error) {
-      throw error;
+    return response.data;
+  } catch (error) {
+    throw error;
   }
 }
 
-function showMessage(data: Array<string>){
-    const apiResponseDiv = document.getElementById('api-response') as HTMLDivElement;
-    const [status, message, cause] = data; // Response from API is an array that has 3 elements: status, message and cause
-    apiResponseDiv.classList.add(status);
-    apiResponseDiv.innerHTML = message;
-    if(status === 'error'){
-        const causeElement = document.getElementById(cause) as HTMLElement;
-        causeElement.style.outline = '3px solid red';
-    }
+function showMessage(data: Array<string>) {
+  const [status, message, cause] = data; // Response from API is an array that has 3 elements: status, message and cause
+  apiResponseDiv.classList.add(status);
+  apiResponseDiv.innerHTML = message;
+  if (status === "error") {
+    const causeElement = document.getElementById(cause) as HTMLElement;
+    causeElement.style.outline = "3px solid red";
+  }
 }
 
 export function handleButtonClick(requestType: string) {
-    const formData = new FormData();
-    formData.set('request', requestType);
-    const response = sendApiRequest(url, formData);
-    response.then(showMessage);
-    formData.delete('request');
+  const loader = document.querySelector(".loader") as HTMLDivElement;
+  apiResponseDiv.innerHTML = "";
+  loader.style.display = "flex";
+  const formData = new FormData();
+  formData.set("request", requestType);
+  const url = requestType === "get-todo" ? todoURL : customURL;
+  const response = sendApiRequest(url, formData);
+  response.then(showMessage);
+  response.finally(() => {
+    loader.style.display = "none";
+  });
+  formData.delete("request");
 }
